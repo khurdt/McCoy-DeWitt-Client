40017d50643bcd1589502cd394bbac0e2262bbc9
khurdt <joshuahurdt@gmail.com>
2022-11-18 03:19:27 -0600

    minor update

--- src/components/projectView-component/CloudinaryUploadWidget.js
+++ src/components/projectView-component/CloudinaryUploadWidget.js
@@ -31,7 +31,7 @@ class CloudinaryUploadWidget extends Component {
                 cloudName: cloudName,
                 uploadPreset: uploadPreset,
                 // cropping: true, //add a cropping step
-                showAdvancedOptions: true,  //add advanced options (public_id and tag)
+                // showAdvancedOptions: true,  //add advanced options (public_id and tag)
                 sources: ["local", "url"], // restrict the upload sources to URL and local files
                 // multiple: false,  //restrict upload to a single file
                 // folder: this.props.project_id, //upload files to the specified folder
